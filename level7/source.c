fbfbdded60d037bce9db85ff83bf9fc03cb45bc3
Sabri Makni <sabrimakni@gmail.com>
2021-03-05 17:15:45 +0100

    check level7

--- level7/source.c
+++ level7/source.c
@@ -28,7 +28,6 @@ int main(int argc, char const *argv[]){
     strcpy(my_struct2->str, argv[2]);
     // FILE *fd = fopen("/home/user/level7/.pass", "r");
     FILE *fd = fopen("/home/user/level8/.pass", "r");
-    // FILE *fd = fopen("./flag", "r");
     fgets(c, 68, fd);
     puts("~~");
     return (0);
